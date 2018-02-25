dbac6eb8d53c5a1ba09d1234b16fbeefe4f580ac
mferdikurniawan97@gmail.com <mferdikurniawan97@gmail.com>
2018-02-25 18:32:07 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/ferdi/ferdi_1202152160_modul3/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.ferdi.ferdi_1202152160_modul3;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //handle untuk memulai MainActivity
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
